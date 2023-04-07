35dfb0c50bd49f032b667648bbd13fef015c39a7
AVolodin <avolodin@polygant.ru>
2023-04-07 12:18:51 +0300

    OC-105

--- core/consts/currencies.py
+++ core/consts/currencies.py
@@ -4,11 +4,17 @@ from collections.abc import Callable
 
 from core.currency import Currency, TokenParams, CoinParams
 
+
 @dataclass
 class BlockchainAccount:
+    """
+    Blockchain Account Info
+    """
     address: str
     private_key: str
+    public_key: Optional[str] = None
     redeem_script: Optional[str] = None
+
 
 ALL_CURRENCIES: List[Currency] = []  # all Currency instances
 
@@ -23,11 +29,19 @@ ALL_TOKEN_CURRENCIES: List[Currency] = []
 
 # {<Currency>: <validation_fn>} - for coins
 # {<Currency>: {<Currency>: <validation_fn>}} - for tokens
-CRYPTO_ADDRESS_VALIDATORS: Union[Dict[Currency, Callable], Dict[Currency, Dict[str, Callable]]] = {}
+CRYPTO_ADDRESS_VALIDATORS: Union[
+    Dict[Currency, Callable],
+    Dict[Currency, Dict[str, Callable]],
+    dict
+] = {}
 
 # {<Currency>: <wallet_creation_fn>} - for coins
 # {<Currency>: {<Currency>: <wallet_creation_fn>}} - for tokens
-CRYPTO_WALLET_CREATORS: Union[Dict[Currency, Callable], Dict[Currency, Dict[str, Callable]]] = {}
+CRYPTO_WALLET_CREATORS: Union[
+    Dict[Currency, Callable],
+    Dict[Currency, Dict[str, Callable]],
+    dict
+] = {}
 
 CRYPTO_COINS_PARAMS: Dict[Currency, CoinParams] = {}
 
